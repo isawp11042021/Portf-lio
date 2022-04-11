e27678cd814d4b992dfe38e284683659b790698d
Isabela Iahn da Silva <102532060+isawp11042021@users.noreply.github.com>
2022-04-11 10:58:06 -0300

    Metade da atividade de fixação, irmão jorel

---
+++ Atividade fixação.java
@@ -0,0 +1,13 @@
+Crie os diretórios irmao_jorel_familia e irmao_jorel_amigos no caminho absoluto : ~/Desktop
+isa@isa-VirtualBox:~/Área de trabalho$ mkdir irmao_jorel_amigos irmao_jorel_familia
+
+Crie o arquivo irmao_jorel no caminho absoluto : ~/Desktop
+isa@isa-VirtualBox:~/Área de trabalho$ touch irmao_jorel
+
+Entre no diretório irmao_jorel_familia.
+isa@isa-VirtualBox:~ cd irmao_jorel_familia
+isa@isa-VirtualBox:~irmao_jorel_familia
+
+Utilizando caminho relativo, com apenas UM comando vá para o diretório irmao_jorel_amigos.
+isa@isa-VirtualBox:~irmao_jorel_familia$ cd../irmao_jorel_amigos
+isa@isa-VirtualBox:~irmao_jorel_amigos$
